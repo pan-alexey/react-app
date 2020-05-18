aa1b937b6b904dc1dd137ab82a0f73ef9e88f0ac
pan-alexey <pan-alexey@live.com>
2020-05-18 08:08:11 +0300

    add webpack config

--- script/webpack/webpack.server.js
+++ script/webpack/webpack.server.js
@@ -17,6 +17,32 @@ module.exports = {
         use: ['babel-loader', 'ts-loader'],
         exclude: /node_modules/,
       },
+
+      {
+        test: /\.(scss|sass|css)$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          // 'css/locals?module&localIdentName=[name]__[local]___[hash:base64:5]',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                auto: /\.module\.\w+$/i,
+                mode: 'local',
+                // exportGlobals: true,
+                localIdentName: '[local]-[hash:base64:5]', // 
+                hashPrefix: 'my-custom-hash',
+              },
+            },
+          },
+          'sass-loader',
+        ],
+      },
+
+
+
+
     ],
   },
 
--- server/server.tsx
+++ server/server.tsx
@@ -1,7 +1,8 @@
 import express from 'express';
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { renderToString } from 'react-dom/server';
+
+import App from '../src/App';
 
 const port = 3000;
 const app = express();
@@ -9,7 +10,7 @@ const app = express();
 app.use(express.static('dist'));
 
 app.get('*', (req, res) => {
-  console.log(renderToString(<div>1</div>));
+  console.log(renderToString(<App />));
   // let content = renderToString(<div>1</div>);
   // res.send(content);
 })
